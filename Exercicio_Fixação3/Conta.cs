6fb842ced67df1f081d8330470bbefc70456eac7
Silas Salema <silazdasilva20@gmail.com>
2019-12-20 10:43:35 -0300

    Conta

---
+++ Exercicio_Fixação3/Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicio_Fixação3
+{
+    class Conta
+    {
+        public int Numero { get;  set; }
+        public string Nome { get;  set; }
+        public double Saldo { get; private set; }
+
+        
+        public Conta(){}
+
+        public void Verificacao (char resposta)
+        {
+            if (resposta == 's' || resposta == 'S')
+            {
+                Console.Write("Entre o com valor do deposito inicial: ");
+                DepositoInicial (double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture));
+            }
+          
+        }
+        public void DepositoInicial (double inicio)
+        {
+            
+            Saldo += inicio;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+        public void Saque(double valor)
+        {
+            Saldo = Saldo - 5.00 - valor;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Nome + ", Saldo: $" + Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Exercicio_Fixação3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Fixação3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta c1 = new Conta();
+            Console.Write("Entre com o número da conta: ");
+            c1.Numero = int.Parse (Console.ReadLine());
+            Console.Write("Entre com o nome do Titular: ");
+            c1.Nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            c1.Verificacao(char.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados Conta:");
+            Console.WriteLine(c1);
+
+            Console.Write("Entre com um valor para depósito: ");
+            c1.Deposito(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(c1);
+            Console.Write("Entre com um valor para saque: ");
+            c1.Saque(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine(c1);
+        }
+    }
+}
